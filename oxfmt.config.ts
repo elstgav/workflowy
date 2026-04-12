143bc87569f99795f1f44bbc35c25419eec17883
Gavin Elster <gavin.elster@me.com>
2026-04-11 21:23:00 -0700

    Enable import sorting

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -4,4 +4,6 @@ import type { OmitIndexSignature } from 'type-fest'
 export default defineConfig({
   semi: false,
   singleQuote: true,
+
+  sortImports: true,
 } as const satisfies OmitIndexSignature<OxfmtConfig>)
